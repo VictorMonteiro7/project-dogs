be4c84d35cda17fc94b860f6491fb9fdc01d4917
Victor Monteiro <victormonteiro@MacBook-Pro-de-Victor.local>
2021-05-23 21:35:54 -0300

    CSS resets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Login from './Components/Login/Login';
+import Home from './Pages.js/Home';
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../Styles/Footer.module.css';
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h3>footer</h3>
+    </div>
+  );
+};
+
+export default Footer;
